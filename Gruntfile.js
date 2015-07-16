22bc00adefde23eb6b3889e0d584ec8a24a2d576
Trevor B <trevor@mtaonline.net>
2015-07-16 12:27:19 -0800

    Add gitignore, Gruntfile, package.json minHtml folder

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        uglify: {
+            jscript: {
+                files: {
+                    'js/js.min.js': ['js/perfmatters.js']
+                }
+            },
+            mainHtml: {
+                files: {
+                }
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    'css/style.min.css': ['css/style.css'],
+                    'css/print.min.css': ['css/print.css']
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                files: [
+                    {
+                        src: 'index.html',
+                        dest: 'minHTML/index.html'
+                    },
+                    {
+                        src: 'project-2048.html',
+                        dest: 'minHTML/project-2048.html',
+                    },
+                    {
+                        src: 'project-mobile.html',
+                        dest: 'minHTML/project-mobile.html'
+                    },
+                    {
+                        src: 'project-webperf.html',
+                        dest: 'minHTML/project-webperf.html'
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    //Default task(s).
+    grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin']);
+};
